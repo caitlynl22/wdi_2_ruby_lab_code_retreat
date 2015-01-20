d172ec5a092c0f6f809c0faee328012e32d8a289
Caitlyn Landry <caitlyn.landry@gmail.com>
2015-01-20 17:24:31 -0500

    Add flat exercise

--- spec/conway_spec.rb
+++ spec/conway_spec.rb
@@ -1,1 +1,21 @@
 require_relative '../lib/conway'
+require 'spec_helper'
+require 'pry'
+require 'byebug'
+
+describe Cell do
+  before (:each) do
+    @cell = Cell.new({status: :dead})
+    test_nesting = [
+      [Cell.new({status: :dead}),
+      Cell.new({status: :alive}),
+      Cell.new({status: :alive})],
+      [Cell.new({status: :dead}),
+      Cell.new({status: :alive}),
+      Cell.new({status: :alive})],
+      [Cell.new({status: :dead}),
+      Cell.new({status: :alive}),
+      Cell.new({status: :dead})]
+    ]
+  end
+end
