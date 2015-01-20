85f2ff8508ae9dc792919db98f3cd0ef2ab256a6
Caitlyn Landry <caitlyn.landry@gmail.com>
2015-01-20 16:34:21 -0500

    Add Silent Coder exercise

---
+++ lib/conway.rb
@@ -0,0 +1,8 @@
+class CellMap
+
+  def initialize(cell_map)
+    @cell_map = cell_map
+  end
+
+
+end
--- spec/conway_spec.rb
+++ spec/conway_spec.rb
@@ -1,1 +1,11 @@
 require_relative '../lib/conway'
+require 'spec_helper'
+
+describe CellMap do
+
+  before(:all) do
+    @cell_map = CellMap.new([[1,0,1],[0,0,1],[1,1,0]])
+  end
+
+
+end
